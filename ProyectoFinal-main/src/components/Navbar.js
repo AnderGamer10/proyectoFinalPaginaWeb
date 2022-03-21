b425f46fa01e0af80417e0967cad5bfba8ad7c1a
AnderGamer10 <anderedondo10@hotmail.com>
2022-03-21 12:14:09 +0100

    v0.1.40 header terminado

--- ProyectoFinal-main/src/components/Navbar.js
+++ ProyectoFinal-main/src/components/Navbar.js
@@ -4,34 +4,40 @@ import menuicon from "../img/lista.png";
 import { Outlet, Link } from "react-router-dom";
 import users from "../data/usuarios.json";
 const Navbar = () => {
-  /*Logica*/
-  return (
-    <div>
-      <nav>
-        <input type="checkbox" id="check" />
-        <label htmlFor="check" className="checkbtn">
-          <img alt="" id="menuicon" src={menuicon} />
-        </label>
-        <label className="logo">MALABARJU</label>
-        <ul>
-          <li>
-            <Link to="/">Inicio</Link>
-          </li>
-          <li>
-            <Link to="/catalogue">Catologo</Link>
-          </li>
-          <li>
-            {users[0].user === "Adrian" ? (
-              <Link to={`/profile/${users[0].user}`}>Perfil</Link>
-            ) : (
-              <Link to="/login">Iniciar Sesión</Link>
-            )}
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </div>
-  );
+
+    return (
+        <div><div id="headerHeight">
+            <header className="posSticky">
+                <nav>
+                    <input type="checkbox" id="check" />
+                    <label htmlFor="check" className="checkbtn">
+                        <img alt="" id="menuicon" src={menuicon} />
+                    </label>
+                    <label className="logo">MALABARJU</label>
+                    <ul>
+                        <li>
+                            <Link to="/">Inicio</Link>
+                        </li>
+                        <li>
+                            <Link to="/catalogue">Catalogo</Link>
+                        </li>
+                        <li>
+                            {users[0].user === "Adrian" ? (
+                                <Link to={`/profile/${users[0].user}`}>Perfil</Link>
+                            ) : (
+                                <Link to="/login">Iniciar Sesión</Link>
+                            )}
+                        </li>
+                        <li>
+                            <Link to="/Login">Log in</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <Outlet />
+        </div>
+        </div>
+    );
 };
 
 export default Navbar;
